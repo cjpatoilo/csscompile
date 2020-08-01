1bdbea87fdc9385ae4ee1fa4b5192043c10c0e85
CJ Patoilo <cjpatoilo@gmail.com>
2020-08-01 04:09:09 -0300

    Move files to root

--- cli.js
+++ cli.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 'use strict'
-const csscompile = require('../')
-const pkg = require('../package.json')
+const csscompile = require('.')
+const pkg = require('./package.json')
 const argv = process.argv.slice(2)
 
 if (argv.indexOf('--version') !== -1 || argv.indexOf('-v') !== -1) {
-	console.log(pkg.version)
-	process.exit(0)
+  console.log(pkg.version)
+  process.exit(0)
 }
 
 if (argv.indexOf('--help') !== -1 || argv.indexOf('-h') !== -1) {
-	console.log(`
+  console.log(`
   Usage:
 
     $ csscompile <input> [<output>] [<options>]
@@ -24,8 +24,8 @@ if (argv.indexOf('--help') !== -1 || argv.indexOf('-h') !== -1) {
 
     $ csscompile src
     $ csscompile src dist
-	`)
-	process.exit(0)
+`)
+  process.exit(0)
 }
 
 csscompile(argv[0], argv[1])
